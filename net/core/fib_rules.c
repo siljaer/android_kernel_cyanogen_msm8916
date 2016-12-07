211ae8886e32da7096f207429d296cb7de6d6792
siljaer <enricovkon@gmail.com>
2016-12-07 18:05:12 +0100

    fixes compilation with ubport kernel conf
    from mainline kernel

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -36,8 +36,6 @@ int fib_default_rule_add(struct fib_rules_ops *ops,
 	r->pref = pref;
 	r->table = table;
 	r->flags = flags;
-	r->uid_start = INVALID_UID;
-	r->uid_end = INVALID_UID;
 	r->fr_net = hold_net(ops->fro_net);
 
 	/* The lock is not required here, the list in unreacheable
